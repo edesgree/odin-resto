81c30b51e4a0dd13ca32673e8cac63fee9c94847
Edouard Desgrée <edouard.desgree@gmail.com>
2022-11-17 23:26:02 +0100

    setup package.json and webpack config.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './sass/style.scss';
+// app
+const app = () => {
+    console.log("hello543");
+    const content = document.getElementById('content');
+    content.innerHTML = 'helo2';
+}
+
+//play app
+app();
