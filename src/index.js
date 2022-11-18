2443bf2de1a0afa3c232a29672db6d95c7188e81
Edouard Desgrée <edouard.desgree@gmail.com>
2022-11-18 01:37:34 +0100

    home page html setup

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import './sass/style.scss';
 // app
 const app = () => {
     console.log("hello543");
-    const content = document.getElementById('content');
-    content.innerHTML = 'helo2';
+    const content = document.getElementById('content1');
+    content.innerHTML = 'helo24';
 }
 
 //play app
